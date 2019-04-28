69bd75212da71d3b2999988f2411853b9ad8518b
Gent <gberani@mobiquityinc.com>
2019-04-28 23:44:59 +0200

    fix: changing github name to dockerhub name

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('tneginareb/docker-nodejs-demo')
+            repositoryName('gentberani/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
